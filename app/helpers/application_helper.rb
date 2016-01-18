e9ec97e33105e33bcccfe6496fcd9b56e476e3ff
Ilton Garcia <ton.garcia.jr@gmail.com>
2016-01-18 18:53:58 -0200

    back_button refactored to be easier to use

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,4 +26,21 @@ module ApplicationHelper
     return 'Sim' if value == true
     return 'Não' if value == false
   end
+
+  # Generate BackLink
+  def gen_back_link(current_obj)
+    if @pitch
+      if current_obj.id.nil?
+        pitch_path @pitch
+      else
+        send("pitch_#{current_obj.class.name.downcase}_path", @pitch, current_obj)
+      end
+    elsif @start_up
+      if current_obj.id.nil?
+        start_up_path @start_up
+      else
+        send("start_up_#{current_obj.class.name.downcase}_path", @start_up, current_obj)
+      end
+    end
+  end
 end
