8c0e349de9c1868adcdf5a010ed8f4119edb0ed7
Ilton Garcia <ton.garcia.jr@gmail.com>
2016-01-28 15:01:01 -0200

    startupers landing finished with quotes

---
+++ app/helpers/quotes_helper.rb
@@ -0,0 +1,12 @@
+module QuotesHelper
+  def quotes
+    [
+        {author:'Walt Disney', text: 'Se você pode sonhá-lo,você pode fazê-lo.'},
+        {author:'Santos Duomont', text: 'As inveções são o resultado de um trabalho teimoso.'},
+        {author:'Bill Gates', text: 'Eu falhei em algumas Provas, Meu colega, pelo contrário, passou em todas. Hoje ele é Engenheiro na Microsft.'},
+        {author:'Steve Jobs', text: 'Você pode encarar um erro como uma besteira a ser esquecida, ou como um resultado que aponta uma nova direção.'},
+        {author:'Jorge Lemann', text: 'Os empreendedores salvarão o Brasil. Só peço que eles não desanimem na primeira dificuldade e que continuem.'},
+        {author:'Nelson Mandela', text: 'Tudo é IMPOSSÍVEL. Até que ACONTEÇA.'},
+    ]
+  end
+end
