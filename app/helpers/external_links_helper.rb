1c049e8621b9be2084504b7098563097d3688126
Ilton Garcia <ton.garcia.jr@gmail.com>
2016-02-03 15:37:31 -0200

    landing page refactored

--- app/helpers/external_links_helper.rb
+++ app/helpers/external_links_helper.rb
@@ -6,4 +6,22 @@ module ExternalLinksHelper
   def face_oauth_url
     user_omniauth_authorize_path('facebook')
   end
+
+  def blog_startupers_landing_links
+    [
+        {href:'#', content: 'Entenda o que é Startup'},
+        {href:'#', content: 'Entenda Co-Criação com Participação nos Lucros'},
+        {href:'#', content: 'Descubra que Startup independe da Área de Atuação, Formação ou Tipo de Experiência Profissional'},
+        {href:'#', content: 'Trabalhe no conforto da sua Casa ou em nossos Espaços de Co-Working (Sem Participação no Lucro)'},
+    ]
+  end
+
+  def blog_investors_landing_links
+    [
+        {href:'#', content: 'Entenda o Co-Investimento'},
+        {href:'#', content: 'Conheça o Investimento Anjo'},
+        {href:'#', content: 'Conheça o Seguro NITRO para Investidores'},
+        {href:'#', content: 'Entenda o Co-Investimento'},
+    ]
+  end
 end
